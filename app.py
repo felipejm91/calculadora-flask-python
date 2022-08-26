702900c0c4a47bf579ce4808fd8dfa45ef0477fd
Felipe <flmeneguetti@yahoo.com.br>
2022-08-26 14:46:43 -0300

    Primeira versão funcional

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from controllers import calculadora
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def calc():
+    if request.method == 'POST':
+        n1 = request.form.get('numero1')
+        n2 = request.form.get('numero2')
+
+        if request.form.get('soma'):
+            resultado = calculadora.soma(n1, n2)
+        elif request.form.get('subtracao'):
+            resultado = calculadora.subtracao(n1, n2)
+        elif request.form.get('divisao'):
+            resultado = calculadora.divisao(n1, n2)
+        elif request.form.get('multiplicacao'):
+            resultado = calculadora.multiplicacao(n1, n2)
+    return render_template('calculadora.html', resultado=resultado)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ controllers/calculadora.py
@@ -0,0 +1,18 @@
+def soma(n1=0, n2=0):
+    resultado = float(n1) + float(n2)
+    return resultado
+
+
+def subtracao(n1=0, n2=0):
+    resultado = float(n1) - float(n2)
+    return resultado
+
+
+def multiplicacao(n1=0, n2=0):
+    resultado = float(n1) * float(n2)
+    return resultado
+
+
+def divisao(n1=0, n2=0):
+    resultado = float(n1) / float(n2)
+    return resultado
